9fa7d5206da24ae211816e4b71d1b4f09b0c64bf
asoluwaseun <soadesina71@student.lautech.edu.ng>
2019-06-02 16:26:13 +0100

    Configure Environment Variables

--- config/db/dbconn.js
+++ config/db/dbconn.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createPool({
 	host     : process.env.DB_HOST,
-	user     : process.env.DB_USER,
+	user     : process.env.DB_USERNAME,
 	password : process.env.DB_PASSWORD,
 	database : process.env.DB_NAME,
 	connectionLimit: 100
