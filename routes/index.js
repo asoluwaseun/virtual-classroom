2aad4a525fe9f25d93e16d73368865d1faffba59
asoluwaseun <soadesina71@student.lautech.edu.ng>
2019-06-02 16:13:07 +0100

    Virtual-Class

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.send(`
-    <!DOCTYPE html>
-<form method="post" action="/api/admin/new" enctype="multipart/form-data">
-  Name  <input type="text" name="name"></br>
-  Email <input type="text" name="email"><br>
-  Department <input type="text" name="department"><br>
-  Level <input type="text" name="level"><br>
-  Picture <input type="file" name="picture"><br>
-  Role <input type="text" name="role">
-  <input type="submit" value="submit">
-</form>`)
+    res.send(`Welcome to Virtual Classroom`)
 });
 
 
